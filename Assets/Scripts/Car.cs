a61c0413f1503ad04149a409523620782896cc7d
prestoplayzsz <125407949+prestoplayzsz@users.noreply.github.com>
2023-08-26 20:50:13 +1200

    Changed code to make it more efficient

--- Assets/Scripts/Car.cs
+++ Assets/Scripts/Car.cs
@@ -5,53 +5,37 @@ using UnityEngine;
 public class Car : MonoBehaviour
 { 
     public Transform centerOfMass;
-
-    public WheelCollider wheelColliderLeftFront;
-    public WheelCollider wheelColliderRightFront;
-    public WheelCollider wheelColliderLeftBack;
-    public WheelCollider wheelColliderRightBack;
-
-    public Transform wheelLeftFront;
-    public Transform wheelRightFront;
-    public Transform wheelLeftBack;
-    public Transform wheelRightBack;
-
     public float motorTorque = 100f;
     public float maxSteer = 20f;
+    
+    public float Steer { get; set; }
+    public float Throttle { get; set; }
+
+
     private Rigidbody _rigidbody;
+    private Wheel[] wheels;
 
     void Start()
     {
+        wheels = GetComponentsInChildren<Wheel>();
         _rigidbody = GetComponent<Rigidbody>();
         _rigidbody.centerOfMass = centerOfMass.localPosition;
     }
 
     void FixedUpdate()
     {
-        wheelColliderLeftBack.motorTorque = Input.GetAxis("Vertical") * motorTorque;
-        wheelColliderRightBack.motorTorque = Input.GetAxis("Vertical") * motorTorque;
-        wheelColliderLeftFront.steerAngle = Input.GetAxis("Horizontal") * maxSteer;
-        wheelColliderRightFront.steerAngle = Input.GetAxis("Horizontal") * maxSteer;
+        
     }
     void Update()
     {
-        var pos = Vector3.zero;
-        var rot = Quaternion.identity;
+        Steer = GameManager.Instance.InputController.SteerInput;
+        Throttle = GameManager.Instance.InputController.ThrottelInput;
 
-        wheelColliderLeftFront.GetWorldPose(out pos, out rot);
-        wheelLeftFront.position = pos;
-        wheelLeftFront.rotation = rot * Quaternion.Euler(0, 0, 0);
+        foreach (var wheel in wheels)
+        {
+            wheel.SteerAngle = Steer * maxSteer;
+            wheel.Torque = Throttle * motorTorque;
+        }
 
-        wheelColliderRightFront.GetWorldPose(out pos, out rot);
-        wheelRightFront.position = pos;
-        wheelRightFront.rotation = rot * Quaternion.Euler(0, 180, 0);
-
-        wheelColliderLeftBack.GetWorldPose(out pos, out rot);
-        wheelLeftBack.position = pos;
-        wheelLeftBack.rotation = rot * Quaternion.Euler(0, 0, 0);
-
-        wheelColliderRightBack.GetWorldPose(out pos, out rot);
-        wheelRightBack.position = pos;
-        wheelRightBack.rotation = rot * Quaternion.Euler(0, 180, 0);
     }
 }
