f498a921f33d97153aaa64bd0697ce8c14d7058b
prestoplayzsz <125407949+prestoplayzsz@users.noreply.github.com>
2023-08-26 22:23:00 +1200

    Added camera follow

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform Target;
+
+    public Vector3 offset;
+    public Vector3 eurlerRotation;
+    public float damper;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.eulerAngles = eurlerRotation;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Target == null) 
+            return;
+
+        transform.position = Vector3.Lerp(transform.position, Target.position + offset, damper * Time.deltaTime);
+    }
+}
