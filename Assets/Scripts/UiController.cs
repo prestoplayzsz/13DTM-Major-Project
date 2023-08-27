5072f04dbe503ac531a101b5450df51527ad7d0b
prestoplayzsz <125407949+prestoplayzsz@users.noreply.github.com>
2023-08-27 18:25:34 +1200

    Adeed Best and Last Lap times

--- Assets/Scripts/UiController.cs
+++ Assets/Scripts/UiController.cs
@@ -36,5 +36,17 @@ public class UiController : MonoBehaviour
             currentLapTime = UpdateUIForPlayer.CurrentLapTime;
             UITextCurrentTime.text = $"Time: {(int)currentLapTime / 60}:{(currentLapTime) % 60:00.000}";
         }
+
+        if (UpdateUIForPlayer.LastLapTime != lastLapTime)
+        {
+            lastLapTime = UpdateUIForPlayer.LastLapTime;
+            UITextLastLapTime.text = $"Last: {(int)lastLapTime / 60}:{(lastLapTime) % 60:00.000}";
+        }
+
+        if (UpdateUIForPlayer.BestLapTime != bestLapTime)
+        {
+            bestLapTime = UpdateUIForPlayer.BestLapTime;
+            UITextBestLapTime.text = bestLapTime < 1000000 ? $"Best: {(int)bestLapTime / 60}:{(bestLapTime) % 60:00.000}" : "BEST: NONE";
+        }
     }
 }
